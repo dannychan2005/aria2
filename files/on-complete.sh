b91f514bac93a2849dedc487ad1009e708f6e631
Dan <dannychan2005@gmail.com>
2018-05-29 22:22:56 +0800

    Update on-complete.sh

--- files/on-complete.sh
+++ files/on-complete.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+rootPath=/aria2
+
 if [ $2 -eq 1 ]; then
-	mv "$3" /data
+	mv "$3" $rootPath/data
 fi
-echo [$(date)] $2, $3, $1 "<br>" >> /data/_log.html
+echo [$(date)] $2, $3, $1 "<br>" >> $rootPath/data/_log.html
